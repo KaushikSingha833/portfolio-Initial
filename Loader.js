b2e8e8fc7735759f30838c2dfbb210d00a150a8e
KaushikSingha833 <kaushiksingha081@gmail.com>
2025-09-14 19:20:02 +0530

    Update Loader.js

--- Loader.js
+++ Loader.js
@@ -1,11 +1,12 @@
-window.addEventListener('load', function() {
+window.addEventListener('pageshow', function(event) {
     const loader = document.getElementById('loading-overlay');
-    
+
+    // Hide the loader immediately
     loader.style.transition = 'opacity 0.5s ease';
     loader.style.opacity = '0';
     setTimeout(() => {
       loader.style.display = 'none';
-    }, 500); 
+    }, 500);
   });
 
   
@@ -20,4 +21,5 @@ window.addEventListener('load', function() {
         window.location.href = href;
       }, 1000);
     });
+
   });
